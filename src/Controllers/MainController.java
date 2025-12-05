2b5f809230f6e87096c5a031cda3e335fe2521f2
bebiandpnda <panidagadeka@gmail.com>
2025-12-06 03:37:09 +0700

    My Second commit

--- src/Controllers/MainController.java
+++ src/Controllers/MainController.java
@@ -7,7 +7,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
